8b67a325363740cc9687f1f436605374b0ab331c
CaglarMacha <91030209+CaglarMacha@users.noreply.github.com>
2022-01-30 02:44:07 +0300

    Update Trigger.sql
    was fixed

--- Trigger.sql
+++ Trigger.sql
@@ -2,5 +2,7 @@ create Trigger kontenjan_azalt on Students
 after insert
 as
 begin
-	update Educations set Capacity=Capacity-1
+	declare @EduId int
+	select EducationId=@EduId from inserted
+	update Educations set Capacity=Capacity-1 where ID=@EduId
 end
